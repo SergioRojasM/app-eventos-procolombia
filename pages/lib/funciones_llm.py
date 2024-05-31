6c1c24300d394397c45814e641febf88c9dbd1ed
Sergio Rojas <sergio.rojas@wom.co>
2024-05-31 11:02:16 -0500

    2024/05/31-02

--- pages/lib/funciones_llm.py
+++ pages/lib/funciones_llm.py
@@ -108,40 +108,40 @@ def consulta_llm(prompt, llm_model, temp, parser, schema):
     tamano_token = obtener_tamano_tokens(prompt)
     
     if llm_model == "GEMINI":
-        # try:
+        try:
             
-        llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=temp, 
-                                    safety_settings={
-                                        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-                                        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-                                        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                                        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE})
-        response = llm.invoke(prompt)
-        result = parser.parse(response.content)
-        result = schema(**result)
-        status = "Ok"
-        # except Exception as e:
-        #         status = e
+            llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=temp, 
+                                        safety_settings={
+                                            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+                                            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                                            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                                            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE})
+            response = llm.invoke(prompt)
+            result = parser.parse(response.content)
+            result = schema(**result)
+            status = "Ok"
+        except Exception as e:
+                status = e
         
     elif llm_model == "GROQ-LLAMA3":
-        # try:
-        llm = ChatGroq(temperature=temp, model_name="llama3-70b-8192")
-        structured_groq_llm = llm.with_structured_output(schema, method="json_mode")
-        result = structured_groq_llm.invoke(prompt)
-        # result = result.__dict__
-        status = "Ok"
-        # except Exception as e:
-        #     status = e
+        try:
+            llm = ChatGroq(temperature=temp, model_name="llama3-70b-8192")
+            structured_groq_llm = llm.with_structured_output(schema, method="json_mode")
+            result = structured_groq_llm.invoke(prompt)
+            # result = result.__dict__
+            status = "Ok"
+        except Exception as e:
+            status = e
 
     elif llm_model == "GROQ-MIXTRAL":
-        # try:
-        llm = ChatGroq(temperature=temp, model_name="mixtral-8x7b-32768")
-        structured_groq_llm = llm.with_structured_output(schema, method="json_mode")
-        result = structured_groq_llm.invoke(prompt)
-        # result = result.__dict__
-        status = "Ok"
-        # except Exception as e:
-        #     status = e
+        try:
+            llm = ChatGroq(temperature=temp, model_name="mixtral-8x7b-32768")
+            structured_groq_llm = llm.with_structured_output(schema, method="json_mode")
+            result = structured_groq_llm.invoke(prompt)
+            # result = result.__dict__
+            status = "Ok"
+        except Exception as e:
+            status = e
 
     return status, result, tamano_token, tamano_palabras
 
@@ -158,15 +158,15 @@ def crear_prompt(tipo, contexto ,  formato_salida):
         event_type: Optional[str] = Field('None', description="describes the event type including congress, symposium, conference, assembly, meeting, summit or seminary",
                                         enum= ['Congress','Symposium','Conference','assembly','meeting','summit','seminary', 'Other'])
         title: Optional[str] = Field('None', description="The name of the event, dont use Acronyms, dont use colon punctuation")
-        # general_title: Optional[str] = Field(None,description="The name of the event, dont use Acronyms, don't use colon punctuation, don't specify the version of the event")
+        general_title: Optional[str] = Field(None,description="The name of the event, dont use Acronyms, don't use colon punctuation, don't specify the version of the event")
         date: Optional[str] = Field('None', description="The Date of the event, dont use colon punctuation") 
         year: Optional[str] = Field('None', description="The year of the event, only one year. if not sure use")
-        # description: Optional[str] = Field(None,description="Summary of the event with details of the event")
+        description: Optional[str] = Field(None,description="Summary of the event with details of the event")
         country: Optional[str] = Field('None',description="The location of the event, if not sure use ")
         city: Optional[str] = Field('None',description="The city of the event, if not sure use ")
-        # place: Optional[str] = Field(None,description="The name of the place where the event takes place, if not sure use None")
-        # key_words: Optional[str] = Field(None,description="Only five key words of thats describe de event, separated by comma, if not sure use None")
-        # asistants: Optional[str] = Field(None,description="Information about number of asistants to the event, if not sure use None")
+        place: Optional[str] = Field(None,description="The name of the place where the event takes place, if not sure use None")
+        key_words: Optional[str] = Field(None,description="Only five key words of thats describe de event, separated by comma, if not sure use None")
+        asistants: Optional[str] = Field(None,description="Information about number of asistants to the event, if not sure use None")
         event_category: Optional[str] = Field('None', description="describes the category of the event", 
                                         enum= ['Medical Sciences','Science','Social Sciences','Management','Education','Law','Economics','Technology','Industry',
                                                 'Culture & Ideas','Arts','Commerce','Mathematics & Statistics','Safety & Security','Sports & Leisure','Ecology & Environment',
